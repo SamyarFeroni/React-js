262211904ff3db8ec3f357fc6bdedfa32cd702a6
SAMYAR_SR <s.feroni@domil.io>
2024-08-26 12:45:23 +0330

    Init commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,86 @@
 import { useState } from "react";
 import "./App.css";
-import Output from "./Component/Output";
 
 function App() {
   //useState consts
-  const [cakeCount, setCakeCount] = useState(0);
-  const [iceCreamCount, setIceCreamCount] = useState(0);
-  const [showCakeButtons, setShowCakeButtons] = useState(false);
-  const [showIceCreamButtons, setShowIceCreamButtons] = useState(false);
+  const [state, setState] = useState({
+    cakeCount: 20,
+    iceCreamCount: 10,
+    showCakeButtons: false,
+    showIceCreamButtons: false,
+  });
   //functions
 
   //functions of cake
   function plusCakeCount() {
-    setCakeCount((cakeCount) => cakeCount + 1);
+    setState((prevState) => ({
+      ...prevState,
+      cakeCount: prevState.cakeCount + 1,
+    }));
   }
   function minusCakeCount() {
-    if (cakeCount > 0) {
-      setCakeCount((cakeCount) => cakeCount - 1);
+    if (state.cakeCount > 0) {
+      setState((prevState) => ({
+        ...prevState,
+        cakeCount: prevState.cakeCount - 1,
+      }));
     }
   }
   function handleCakeSell() {
-    setShowCakeButtons(true);
+    // setShowCakeButtons(true);
+    setState((prevState) => ({
+      ...prevState,
+      showCakeButtons: true,
+    }));
   }
   //functions of IceCeram
   function plusIceCreamCount() {
-    setIceCreamCount((iceCreamCount) => iceCreamCount + 1);
+    // setIceCreamCount((iceCreamCount) => iceCreamCount + 1);
+    setState((prevState) => ({
+      ...prevState,
+      iceCreamCount: prevState.iceCreamCount + 1,
+    }));
   }
   function minusIceCreamCount() {
-    if (iceCreamCount > 0) {
-      setIceCreamCount((iceCreamCount) => iceCreamCount - 1);
+    if (state.iceCreamCount > 0) {
+      setState((prevState) => ({
+        ...prevState,
+        iceCreamCount: prevState.iceCreamCount - 1,
+      }));
     }
   }
   function handleIceCreamSell() {
-    setShowIceCreamButtons(true);
+    // setShowIceCreamButtons(true);
+    setState((prevState) => ({
+      ...prevState,
+      showIceCreamButtons: true,
+    }));
   }
 
   return (
-    <Output/>
+    <div className="App">
+      <h2>
+        Cake<button onClick={handleCakeSell}>Sell Cake</button>
+        {state.showCakeButtons &&(
+          <>
+            <button onClick={plusCakeCount}>+</button>{" "}
+            <button onClick={minusCakeCount}>-</button>
+          </>
+        )}
+        Count: <span>{state.cakeCount}</span>
+      </h2>
+      <h2>
+        Ice Cream
+        <button onClick={handleIceCreamSell}>Sell Ice Cream</button>
+        {state.showIceCreamButtons && (
+          <>
+            <button onClick={plusIceCreamCount}>+</button>
+            <button onClick={minusIceCreamCount}>-</button>
+          </>
+        )}
+        Count: <span>{state.iceCreamCount}</span>
+      </h2>
+    </div>
   );
 }
 
