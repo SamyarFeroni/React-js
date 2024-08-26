aa296c6757779f31a9056a4045b4fab5dab9fb86
SAMYAR_SR <s.feroni@domil.io>
2024-08-26 09:55:01 +0330

    Init commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import Output from "./Component/Output";
 
 function App() {
   //useState consts
@@ -35,31 +36,7 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <h2>
-        Cake
-        <button onClick={handleCakeSell}>Sell Cake</button>
-        {showCakeButtons && (
-          <>
-            <button onClick={plusCakeCount}>+</button>
-            <button onClick={minusCakeCount}>-</button>
-          </>
-        )}
-        Count: <span>{cakeCount}</span>
-      </h2>
-
-      <h2>
-        Ice Cream
-        <button onClick={handleIceCreamSell}>Sell Ice Cream</button>
-        {showIceCreamButtons && (
-          <>
-            <button onClick={plusIceCreamCount}>+</button>
-            <button onClick={minusIceCreamCount}>-</button>
-          </>
-        )}
-        Count: <span>{iceCreamCount}</span>
-      </h2>
-    </div>
+    <Output/>
   );
 }
 
